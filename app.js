e27f70f57646a91ab96b905fcb3e3c014e3e8e50
MausoofAbdullah <mausoofabdullah@gmail.com>
2023-07-31 17:44:20 +0530

    changed var to const

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 const hbs= require('express-handlebars')  
 const session=require('express-session')
 const mongoose=require('mongoose')
@@ -10,10 +10,10 @@ const mongoose=require('mongoose')
 
 
 // var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/user');
+const usersRouter = require('./routes/user');
 const adminRouter=require('./routes/admin')
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
